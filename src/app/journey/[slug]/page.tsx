62efda0ef5f6b589b5ad39089d048e00eed31448
Neuron Electronics <develop.neuron@gmail.com>
2025-03-08 15:09:29 +0100

    Fix: Type does not satisfy the constraint 'PageProps' (Journey.tsx)

--- src/app/journey/[slug]/page.tsx
+++ src/app/journey/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { getJourney } from "@/app/api/journey";
 import Journey from "./Journey";
 
-export default async function Page({ params, searchParams } : { params: { slug: string }, searchParams: { referringEva: string }}) {
+export default async function Page({ params, searchParams } : { params: Promise<{ slug: string }>, searchParams: Promise<{ referringEva: string }> }) {
   const { slug } = await params;
 
   const dataPromise = getJourney(slug);
