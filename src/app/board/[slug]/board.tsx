6ea90c1d4569a9355517496e2d4ff5e19955417d
Neuron Electronics <develop.neuron@gmail.com>
2025-03-11 11:57:51 +0100

    fix: Added stringify

--- src/app/board/[slug]/board.tsx
+++ src/app/board/[slug]/board.tsx
@@ -6,7 +6,7 @@ import { StationData } from "@/types/timetable";
 export default async function Board({ dataPromise }: { dataPromise: Promise<StationData | null> }) {
   const data = await dataPromise;
   if (!data || !data?.stopGroups) return <div>Fehler beim Laden der Daten</div>;
-  console.log(data)
+  console.log(JSON.stringify(data))
   return (
     <div>
       <Navbar title={data.stationName} referring={'/'} />
