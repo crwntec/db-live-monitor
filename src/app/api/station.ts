265c303497154e9c56e670fa2747ac07da5fe034
Neuron Electronics <develop.neuron@gmail.com>
2025-03-24 18:28:15 +0100

    feat: added DS100 to autcomplete and keyboard navigation

--- src/app/api/station.ts
+++ src/app/api/station.ts
@@ -1,16 +1,17 @@
 import { findStationByEvaId, findStationByDS100 } from '@/lib/stations';
+import { Station } from '@/types/stations';
 import autocomplete from 'db-hafas-stations-autocomplete';
 
-export const getEVAFromDS100 = async (input: string) => {
-    const ds100Pattern = /^[abdefhklmnrstuw]{1}[a-z]|[A-Z]{1,4}$/;
+const ds100Pattern = /^[abdefhklmnrstuw]{1}[a-z]|[A-Z]{1,4}$/;
 
+export const getEVAFromDS100 = async (input: string) : Promise<number[] | null> => {
     if (ds100Pattern.test(input)) {
         try {
             const station = await findStationByDS100(input);
             return station ? [ station.eva, ...station.meta_evas] : null;
         } catch (error) {
             console.error('Error reading stations:', error);
-            return input;
+            return null;
         }
     } else {
         const station = await findStationByEvaId(input);
@@ -19,7 +20,12 @@ export const getEVAFromDS100 = async (input: string) => {
 };
 
 
-export const autoCompleteStation = async (input: string) => {
+export const autoCompleteStation = async (input: string) : Promise<Station[]> => {
+    //Check if input is a DS100 code
+    if (ds100Pattern.test(input)) {
+        const station = await findStationByDS100(input);
+        return station ? [station] : [];
+    }
     const results = await autocomplete(input, 6);
     const mappedResults = [];
     
