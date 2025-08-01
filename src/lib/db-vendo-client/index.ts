ac4b9e896d8c4aeca4b9cc3459959fcbd6f68293
crwntec <develop.neuron@gmail.com>
2025-08-01 18:10:30 -0500

    Added logging to index.ts

--- src/lib/db-vendo-client/index.ts
+++ src/lib/db-vendo-client/index.ts
@@ -1,4 +1,10 @@
-import {createClient} from 'db-vendo-client';
-import {profile} from 'db-vendo-client/p/dbnav/index';
+import { createClient } from "db-vendo-client";
+import { profile } from "db-vendo-client/p/dbnav/index";
+import fs from "fs";
+import path from "path";
+
+const filePath = path.resolve("node_modules/db-hafas-stations/full.ndjson");
+console.log("Checking file:", filePath);
+console.log("File exists:", fs.existsSync(filePath));
 
 export const createVendoClient = () => createClient(profile, "db-live");
