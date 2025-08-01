0d3ab6e5df723680463c2892cdb7b9ff62921731
crwntec <develop.neuron@gmail.com>
2025-08-01 17:57:20 -0500

    fix next.config.mjs build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    outputFileTracingIncludes: {
-      "/api/**/*": ["./node_modules/db-hafas-stations/**/*"],
-      "/journey/**/*": ["./node_modules/db-hafas-stations/**/*"],
-    },
+    outputFileTracing: true,
+  },
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./node_modules/db-hafas-stations/**/*"],
+    "/journey/**/*": ["./node_modules/db-hafas-stations/**/*"],
   },
 };
+
 export default nextConfig;
